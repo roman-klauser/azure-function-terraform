431565f4af37ecc3a2269fbc49eca40aa7bd60c6
Roman Klauser <roman.klauser@codecentric.de>
2021-03-19 14:41:02 +0100

    as

--- hello-world/index.ts
+++ hello-world/index.ts
@@ -2,20 +2,17 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hallo, " + name + "."
-        : "Du bist aber unhöflich.";
+
 
 
 
     context.bindings.bier = JSON.stringify({
-        id: 2, //uuidv4(),
-        value: '98989'
+        id: "22",
+        type: "whaat"
     });
 
     context.res = {
-        body: 'done'
+        body: 'done!'
     };
 
     // context.done()
