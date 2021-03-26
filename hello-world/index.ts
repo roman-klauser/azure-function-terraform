92186106c3618a15291179d8d66ce7450a4a1889
Roman Klauser <roman.klauser@codecentric.de>
2021-03-26 11:34:57 +0100

    removed hard values

--- hello-world/index.ts
+++ hello-world/index.ts
@@ -1,15 +1,11 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
-interface Person {
-    firstName: string;
-    lastName: string;
-}
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    context.bindings.bier = JSON.stringify(req.body);
+    context.bindings.Person = JSON.stringify(req.body);
 
     context.res = {
-        body: 'done!!'
+        body: 'Danke.'
     };
 };
 
