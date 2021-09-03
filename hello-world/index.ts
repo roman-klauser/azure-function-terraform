53e668031a3e1ce28ccf0df8ef696e426e90ed02
Roman Klauser <roman.klauser@codecentric.de>
2021-09-03 10:09:33 +0200

    ..

--- hello-world/index.ts
+++ hello-world/index.ts
@@ -11,3 +11,40 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
 
 export default httpTrigger;
 // func azure functionapp publish ccmsrkazuretf-dev-function-app
+/*
+{
+  "bindings": [
+    {
+      "type": "cosmosDBTrigger",
+      "name": "documents",
+      "direction": "in",
+      "leaseCollectionName": "leases",
+      "connectionStringSetting": "COSMOS_DB_CONNECTION_STRING",
+      "databaseName": "ccmsrk-cosmos-sql-db",
+      "collectionName": "people",
+      "createLeaseCollectionIfNotExists": true
+    },
+    {
+      "type": "eventGrid",
+      "name": "outputEvent",
+      "topicEndpointUri": "MyEventGridTopicUriSetting",
+      "topicKeySetting": "MyEventGridTopicKeySetting",Å
+      "direction": "out"
+    }
+  ]
+}
+
+module.exports = async function (context, documents) {
+    context.log('PersonCreated: ', documents[0]);
+
+    context.bindings.outputEvent = {
+        id: 'message-id',
+        subject: 'subject-name',
+        dataVersion: '1.0',
+        eventType: 'event-type',
+        data: "event-data",
+    };
+
+    context.done()
+}
+* */
